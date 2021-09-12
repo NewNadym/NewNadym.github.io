c4b670b1b5eabdae4222dd0451a16f8ba07ee9ae
Nick_Kovtunov <NickKovtunov@users.noreply.github.com>
2021-09-12 14:14:05 +0300

    Старт

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-main>
+      <div class="mainWrap">
+      <router-view/>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+  html, body{
+    height:100%;
+    overflow: hidden !important;
+    width: 100%;
+  }
+
+  .mainWrap{
+    background: url(assets/images/main.jpg) no-repeat;
+    background-position: center;
+    background-size: cover;
+    height:100%;
+    width:100%;
+  }
+</style>
