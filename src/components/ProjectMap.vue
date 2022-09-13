a78cb5b69a8c2e1ed7d42122e414dd4636d4de8c
Nick_Kovtunov <NickKovtunov@users.noreply.github.com>
2022-09-13 20:03:55 +0500

    Изменил размер модального окна карты

--- src/components/ProjectMap.vue
+++ src/components/ProjectMap.vue
@@ -15,15 +15,15 @@
           Карта
         </v-btn>
       </template>
-      <v-card>
+      <v-card style="height: 90vh;">
         <v-card-title>
           Карта
           <v-spacer></v-spacer>
           <v-icon @click="dialog = false">mdi-close</v-icon>
         </v-card-title>
         <v-divider></v-divider>
-        <v-card-text style="padding: 20px 24px 10px;font-size: 15px;line-height: 1.6;">
-          <div style="position:relative;overflow:hidden;"><iframe src="https://yandex.ru/map-widget/v1/-/CCUm5YHPxD" width="100%" height="400" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe></div>
+        <v-card-text style="padding: 10px;font-size: 15px;line-height: 1.6;">
+          <div style="position:relative;overflow:hidden;height: 100%;"><iframe src="https://yandex.ru/map-widget/v1/-/CCUm5YHPxD" width="100%" height="100%" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe></div>
         </v-card-text>
       </v-card>
     </v-dialog>
